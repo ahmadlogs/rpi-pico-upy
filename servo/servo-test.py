9ac4127d3336184ed5922b71eeb9b7b3d480acc8
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2021-05-11 16:02:29 +0500

    Update servo-test.py

--- servo/servo-test.py
+++ servo/servo-test.py
@@ -1,15 +1,6 @@
 from servo import Servo
 import utime
 
-servo_pwm_freq = 50
-min_u16_duty = 1000 - 400 # offset for correction
-max_u16_duty = 6000  # offset for correction
-min_angle = 0
-max_angle = 180    
-current_angle = 0.001
-
-
-    
 pwmPin=15 #change for your pin
 motor=Servo(pwmPin)
 
